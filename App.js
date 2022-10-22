a9fec85e2b3f118d1cd49980fbc6bf6a0260278d
Andhika Yuana <andhika.yuana@ruangguru.com>
2022-10-22 15:15:49 +0700

    chore: try to use code push

--- App.js
+++ App.js
@@ -3,8 +3,17 @@ import { StyleSheet, Text, TouchableHighlight, View } from 'react-native'
 import { getRandom } from './api/jokesbapack2';
 import Jokes from "./components/Jokes";
 import { actionCreators, initialState, reducer } from './reducers/jokes'
+import CodePush from 'react-native-code-push';
 
-export default function App() {
+const CodePushOptions = {
+  checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
+  mandatoryInstallMode: CodePush.InstallMode.IMMEDIATE,
+  updateDialog: {
+    appendReleaseDescription: true,
+    title: "a new update is available!"
+}}
+
+function App() {
   const [state, dispatch] = useReducer(reducer, initialState)
   const { joke, loading, error } = state
 
@@ -36,12 +45,14 @@ export default function App() {
   )
 }
 
+export default CodePush(CodePushOptions)(App);  
+
 const styles = StyleSheet.create({
   container: {
     width: '100%',
     height: '100%',
     // flex: 1,
-    // backgroundColor: 'white',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
